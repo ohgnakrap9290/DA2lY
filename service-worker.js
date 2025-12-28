77a11b370c515c400ef3287b05c8a69e8b6f13d9
k@NGh5! <phantom4862@naver.com>
2025-12-28 10:18:08 +0900

    1

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'da2ly-cache-v1';
+const FILES = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
